4867c6c61adab147e845a0e0c473f281cdcfbf8f
Victoria Le <leqvictoria@gmail.com>
2023-06-14 10:35:29 -0400

    link supabase and App

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createClient } from '@supabase/supabase-js';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
+const supabase = createClient(
+  "https://gsmnxndgtuyrirhiwqlj.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzbW54bmRndHV5cmlyaGl3cWxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY2Njg1ODYsImV4cCI6MjAwMjI0NDU4Nn0.9sNqyLgivpokVgNayHrJFhvkKMQrQ2uFmj_x18m-kUA"
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Allow the app to talk to supabase in any compononent */}
+    <SessionContextProvider> 
+      <App />
+    </SessionContextProvider>
   </React.StrictMode>
 );
 
