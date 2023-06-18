06e09c38a20c4dfc9a173352115e1e23c3d993ac
Victoria Le <leqvictoria@gmail.com>
2023-06-18 06:59:28 -0400

    error handling and signOut

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ function App() {
   const supabase = useSupabaseClient() //talk to supabase 
 
   async function googleSignIn() {
-    await supabase.auth.signInWithOAuth({
+    const {error} = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: { //scopes allows access to specific part of the provider
-
+        scopes: 'https://www.googleapis.com/auth/calendar'
       }
     })
+    if (error){
+      alert("Error logging in to ther provider with supabse")
+      console.log(error)
+    }
   }
+
+  async function signOut() {
+    await supabase.auth.signOut()
+  }
+  
   return (
     <div className="App">
       <div style={{width:"400px", margin: "30px auto"}}>
