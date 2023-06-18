4ca76339de73e818ca28afbe46089d5024ad34a7
Victoria Le <leqvictoria@gmail.com>
2023-06-18 06:43:52 -0400

    signIn With Google function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 
 function App() {
-  const session = useSession() //tokens 
+  const session = useSession() //tokens, when session exists we have a user  
   const supabase = useSupabaseClient //talk to supabase 
+
+  async function googleSignIn() {
+    await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        //scope allow to access specifc part of the provider, in this case google calendar 
+      }
+    })
+  }
   return (
     <div className="App">
-      
+      <div style={{width:"400px", margin: "30px auto"}}>
+        {/* CHECKING IF SESSION EXISTS */}
+        {session 
+          ? 
+          <>
+            <h2>Hey there, {session.user.email}</h2>
+          </>
+          :
+          <>
+            <button onClick={() => googleSignIn()}>Sign In With Google</button>
+          </>
+        }
+      </div>
     </div>
   );
 }
