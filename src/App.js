931d55360645bd461a855488b2b6a347fb4c9e8f
Victoria Le <leqvictoria@gmail.com>
2023-06-18 07:47:15 -0400

    avoid the flickering issue when reload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useSession, useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react';
 
 function App() {
   const session = useSession() //tokens, when session exists we have a user  
   const supabase = useSupabaseClient() //talk to supabase 
+  const {isLoading} = useSessionContext()
+
+  if(isLoading){ //this is to avoid the web to flickering after reload. Because when refresh aka to context the app original will be null, this is to go around that issue
+    return <></>
+  }
 
   async function googleSignIn() {
     const {error} = await supabase.auth.signInWithOAuth({
@@ -21,7 +26,9 @@ function App() {
   async function signOut() {
     await supabase.auth.signOut()
   }
-  
+
+  console.log(session)
+
   return (
     <div className="App">
       <div style={{width:"400px", margin: "30px auto"}}>
@@ -30,6 +37,7 @@ function App() {
           ? 
           <>
             <h2>Hey there, {session.user.email}</h2>
+            <button onClick={() => signOut()}></button>
           </>
           :
           <>
